33f4674f74d8bee2d5714ac28c10150e66b2fd9a
doni <daniilmirlanuulu@gmail.com>
2022-08-13 00:37:22 +0600

    правка task_3/main.py

--- task_3/main.py
+++ task_3/main.py
@@ -3,13 +3,13 @@
 '''
 
 
-def my_func(n1,n2,n3):
+def my_func(n1, n2, n3):
     new_lis = []
     new_lis.append(n1)
     new_lis.append(n2)
     new_lis.append(n3)
     print(new_lis)
-    new_lis=sorted(new_lis)
+    new_lis = sorted(new_lis)
     return f'{new_lis[-1]} + {new_lis[-2]} = {sum(new_lis[-2::])}'
 
 
